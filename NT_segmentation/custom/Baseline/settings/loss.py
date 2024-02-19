5b7449b6b3d5dfa49e9391439373667510de7761
dlekgo79 <90365178+dlekgo79@users.noreply.github.com>
2024-02-19 15:47:02 +0900

    Delete NT_segmentation directory

--- NT_segmentation/custom/Baseline/settings/loss.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-from monai.losses.dice import DiceLoss, one_hot
-from monai.metrics import DiceMetric
-
-def getLoss():
-	# criterion = nn.CrossEntropyLoss()
-	criterion = DiceLoss()
-
-	return criterion
